4a4fc2490ff79608f7e59f4c9a61ac909206df06
Miguel Angel Hernandez Corona <miguelhc@MacBook-Air-de-Miguel.local>
2024-06-10 20:46:11 -0600

    listo para produccion 6

--- prestamos_api/settings.py
+++ prestamos_api/settings.py
@@ -153,9 +153,9 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = {
-    config('CORS_ALLOWED_ORIGINS', cast=Csv())
-}
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
