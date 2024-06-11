9f9dfc211aa001acaa9304f6b23061b331bbce2d
Miguel Angel Hernandez Corona <miguelhc@MacBook-Air-de-Miguel.local>
2024-06-10 21:50:34 -0600

    listo para produccion 10

--- prestamos/urls.py
+++ prestamos/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include, re_path
 from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'prestamos', views.PrestamoViewSet)
@@ -15,3 +17,6 @@ urlpatterns = [
     re_path('profile', views.profile),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
