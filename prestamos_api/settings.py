21a3d8322a245db66593c70764d50bdb9b37fcfe
Miguel Angel Hernandez Corona <miguelhc@MacBook-Air-de-Miguel.local>
2024-06-10 20:57:50 -0600

    listo para produccion 8

--- prestamos_api/settings.py
+++ prestamos_api/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
     # }
 
     'default': dj_database_url.config(
-        default=config('DATABASE_URL_INTERNA')
+        default=config('DATABASE_URL_EXTERNA')
     )
 }
 
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:5173/",
 ]
 
 REST_FRAMEWORK = {
