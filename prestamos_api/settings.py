0aad3a626678a5cb63dd5da63fb1b7c87d29044f
Miguel Angel Hernandez Corona <miguelhc@MacBook-Air-de-Miguel.local>
2024-06-10 23:12:14 -0600

    listo para produccion 19

--- prestamos_api/settings.py
+++ prestamos_api/settings.py
@@ -149,6 +149,8 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    WHITENOISE_USE_FINDERS = True
+
 
 
 
