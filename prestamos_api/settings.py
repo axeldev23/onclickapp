9352744518bc4d4c3da969a2740d3dfc3ac9ede8
Miguel Angel Hernandez Corona <miguelhc@MacBook-Air-de-Miguel.local>
2024-06-11 19:02:48 -0600

    listo para produccion 27

--- prestamos_api/settings.py
+++ prestamos_api/settings.py
@@ -35,14 +35,7 @@ ALLOWED_HOSTS = [
     config('ALLOWED_HOSTS')
 ]
 
-STORAGES = {
-    'default': {
-        'BACKEND': 'django.core.files.storage.FileSystemStorage',
-    },
-    'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
-    },
-}
+
 
 # Application definition
 
@@ -153,6 +146,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
