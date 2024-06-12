7a28125a3184b75e6d4e3e078a0303cd36333715
Miguel Angel Hernandez Corona <miguelhc@MacBook-Air-de-Miguel.local>
2024-06-11 19:09:50 -0600

    listo para produccion 28

--- prestamos_api/urls.py
+++ prestamos_api/urls.py
@@ -18,16 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('prestamos.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('prestamos.urls'))
+    path('api/', include('prestamos.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
